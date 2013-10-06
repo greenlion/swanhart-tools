b7e06ca180c5aacfb67dbfad6465a0ac49deb9c0
justin <justin@sqtest.sqcentral>
2013-10-05 23:28:29 -0700

    Add new DROP procedure

---
+++ flexviews/procs/drop.sql
@@ -0,0 +1,122 @@
+DELIMITER ;;
+/*  Flexviews for MySQL 
+    Copyright 2008 Justin Swanhart
+
+    FlexViews is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FlexViews is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FlexViews in the file COPYING, and the Lesser extension to
+    the GPL (the LGPL) in COPYING.LESSER.
+    If not, see <http://www.gnu.org/licenses/>.
+*/
+
+DROP PROCEDURE IF EXISTS flexviews.disable;;
+/****f* SQL_API/disable
+ * NAME
+ *   flexviews.disable - Drop the materialized view table.  
+ * SYNOPSIS
+ *   flexviews.disable(v_mview_id);
+ * FUNCTION
+ *   This function drops the table holding the rows for the materialized
+ *   view.  There is no warning and the table is dropped as soon as this command is issued.
+ * INPUTS
+ *   v_mview_id - The materialized view id 
+ * RESULT
+ *   An error will be generated in the MySQL client if the view can not be disabled.
+ * NOTES
+ *   The dictionary information is not removed, instead the metadata is updated to reflect the disabled status.
+ * SEE ALSO
+ *   SQL_API/create, SQL_API/enable, SQL_API/get_id 
+ * EXAMPLE
+ *  mysql>
+ *    call flexviews.disable(flexviews.get_id('test','mv_example'))
+ 
+******
+*/
+
+CREATE DEFINER=`flexviews`@`localhost` PROCEDURE flexviews.`disable`(
+  IN v_mview_id INT UNSIGNED
+)
+  MODIFIES SQL DATA
+  COMMENT 'Disable a materialized view'
+BEGIN
+  -- DECLARE v_mview_enabled tinyint(1);
+  DECLARE v_mview_name TEXT;
+  DECLARE v_mview_schema TEXT;
+  DECLARE v_mview_enabled INT;
+  DECLARE v_child_mview_id INT;
+  DECLARE v_parent_mview_id INT DEFAULT NULL;
+
+  -- backup SESSION max_sp_recursion_depth
+  DECLARE bkp_max_sp_recursion_depth INT UNSIGNED DEFAULT @@session.max_sp_recursion_depth;
+  SET max_sp_recursion_depth := 255;
+
+  START TRANSACTION WITH CONSISTENT SNAPSHOT;
+
+  SELECT mv1.mview_name, 
+         mv1.mview_schema,
+	 mv1.mview_enabled,
+	 mv1.parent_mview_id,
+         mv2.mview_id
+    INTO v_mview_name, 
+         v_mview_schema,
+         v_mview_enabled,
+	 v_parent_mview_id,
+         v_child_mview_id
+    FROM flexviews.mview mv1
+    LEFT JOIN flexviews.mview mv2
+      ON mv1.mview_id = mv2.parent_mview_id
+   WHERE mview_id = v_mview_id;
+
+   IF v_mview_id IS NULL THEN
+     CALL flexviews.signal('The specified materialized view does not exist (NOTHING WAS DROPPED)');
+   END IF;
+
+   IF v_mview_enabled = FALSE OR v_mview_enabled is null THEN
+     CALL flexviews.signal('This materialized view is already disabled (NOTHING WAS DROPPED)');
+   END IF;
+
+   -- start the transaction on the parent view.  the parent and child will both get marked
+   -- disabled when the first child table is dropped, or if there is no child view, when
+   -- the first parent table is dropped
+   IF v_parent_mview_id IS NULL THEN
+     START TRANSACTION;
+   END IF;
+
+   UPDATE flexviews.mview
+      SET mview_last_refresh = NULL,
+          mview_enabled = FALSE
+    WHERE mview_id = v_mview_id;
+
+   IF v_child_mview_id IS NOT NULL THEN
+     CALL flexviews.drop(v_child_mview_id);
+   END IF;
+
+   SET @v_sql = CONCAT('DROP TABLE IF EXISTS ', v_mview_schema, '.', v_mview_name);
+   PREPARE drop_stmt FROM @v_sql; 
+   EXECUTE drop_stmt;
+
+   SET @v_sql = CONCAT('DROP TABLE IF EXISTS ', v_mview_schema, '.', v_mview_name, '_delta');
+   PREPARE drop_stmt FROM @v_sql; 
+   EXECUTE drop_stmt;
+
+   SET @v_sql := NULL;
+   DEALLOCATE PREPARE drop_stmt;
+
+   IF v_child_mview_id IS NOT NULL THEN
+     SELECT 'The view, the view delta and the child views (if any) have now been removed' as `MESSAGE` from dual;
+   END IF;
+ 
+   -- restore SESSION max_sp_recursion_depth
+   SET max_sp_recursion_depth := bkp_max_sp_recursion_depth;
+END ;;
+
+DELIMITER ;
