b362952553759201df116c79990a89f53fb4e3ea
Rick Birch <r.birch@modcloth.com>
2013-10-18 20:02:06 +0000

    add logging

--- flexviews/consumer/consumer_safe.sh
+++ flexviews/consumer/consumer_safe.sh
@@ -2,10 +2,13 @@
 
 while true
 do
-  php run_consumer.php --pid=flexcdc.pid $* > /dev/null 2>&1 < /dev/null
+  #php run_consumer.php --pid=flexcdc.pid $* > /dev/null 2>&1 < /dev/null
+  php run_consumer.php --pid=flexcdc.pid $*  2>&1 >> /root/flex_cdc_logs/flex_cdc_log.log 
   if [ $? -eq 0 ]; then
     exit 0
   fi
   echo "Restarting FlexCDC!"
-  sleep 1
+	echo "Restarting FlexCDC!" | mail dwh_alerts@modcloth.com
+  sleep 1000
+
 done
