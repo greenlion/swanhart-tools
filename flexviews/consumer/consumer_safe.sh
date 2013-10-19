169eaf9d9fbada3e35e489f91e99d10d131eee68
greenlion <justin@sqtest.sqcentral>
2013-10-19 14:19:07 -0700

    Add email, extra logging and variable sleep time before FlexCDC restart to consumer.ini.  Modify consumer_safe.sh appropriately.  Undo some changes from ModCloth in favor of these changes

--- flexviews/consumer/consumer_safe.sh
+++ flexviews/consumer/consumer_safe.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+FAILURE_SLEEP_TIME=$(grep failure_sleep_time consumer.ini|cut -d= -f2)
+FAILURE_ERROR_LOG=$(grep failure_error_log consumer.ini|cut -d= -f2)
+FAILURE_EMAIL_ADDRESS=$(grep failure_email_address consumer.ini|cut -d= -f2)
+
+if [ -z $FAILURE_SLEEP_TIME ]; then
+  FAILURE_SLEEP_TIME=300
+fi
+
+if [ -z $FAILURE_ERROR_LOG ]; then 
+  FAILURE_ERROR_LOG=flex_cdc_log.log
+fi
+
 
 while true
 do
-  #php run_consumer.php --pid=flexcdc.pid $* > /dev/null 2>&1 < /dev/null
-  php run_consumer.php --pid=flexcdc.pid $*  2>&1 >> /root/flex_cdc_logs/flex_cdc_log.log 
+  php run_consumer.php --pid=flexcdc.pid $* 2>&1 >> $FAILURE_ERROR_LOG
   if [ $? -eq 0 ]; then
     exit 0
   fi
-  echo "Restarting FlexCDC!"
-	echo "Restarting FlexCDC!" | mail dwh_alerts@modcloth.com
-  sleep 1000
 
+  if [ ! -z $FAILURE_EMAIL_ADDRESS ]; then
+    FAILURE_EMAIL_BODY=$(tail flexcdc.err)
+    echo $FAILURE_EMAIL_BODY | mail $FAILURE_EMAIL_ADDRESS
+  fi
+
+  echo "Restarting FlexCDC in $FAILURE_SLEEP_TIME seconds!"  
+  sleep $FAILURE_SLEEP_TIME
 done
