9ffbf92e7cfb11b5f06ecebcb153e5dc0729a9c2
Justin <greenlion@gmail.com>
2014-03-28 12:42:55 -0700

    Fix problems with length, make division by zero return null

--- udf/bcmath.c
+++ udf/bcmath.c
@@ -114,7 +114,7 @@ my_bool bcadd_init(UDF_INIT *initid, UDF_ARGS *args, char* message) {
 	return 0;
 }
 
-char *bcadd(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length, char *is_null, char *error) {
+char *bcadd(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long *length, char *is_null, char *error) {
 	bc_num tmpnum1;
 	bc_num tmpnum2;
 	bc_num resultnum;
@@ -154,10 +154,10 @@ char *bcadd(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length
 	bc_add(tmpnum1,tmpnum2,&resultnum,(long)*(args->args[0])); 
 
 	strval = bc_num2str(resultnum);
-	length = strlen(strval);
-	if(length <= 766) {
+	*length = strlen(strval);
+	if(*length <= 766) {
 		memset(result,0,766);
-		strncpy(result,strval,length);
+		strncpy(result,strval,*length);
 		free(strval);
 		strval  = result;
 	}
@@ -169,7 +169,8 @@ char *bcadd(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length
 	return strval;
 }
 
-my_bool bcdiv_init(UDF_INIT *initid, UDF_ARGS *args, char* message) {
+
+my_bool bcpow_init(UDF_INIT *initid, UDF_ARGS *args, char* message) {
 	int i;
 	long scale;
 	if(args->arg_count != 3) {
@@ -191,7 +192,7 @@ my_bool bcdiv_init(UDF_INIT *initid, UDF_ARGS *args, char* message) {
 	return 0;
 }
 
-char *bcdiv(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length, char *is_null, char *error) {
+char *bcpow (UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long *length, char *is_null, char *error) {
 	bc_num tmpnum1;
 	bc_num tmpnum2;
 	bc_num resultnum;
@@ -229,13 +230,99 @@ char *bcdiv(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length
 		bc_str2num(&tmpnum2, str, strlen(p+1));
 	}
 
+	bc_raise(tmpnum1,tmpnum2,&resultnum,(long)*(args->args[0])); 
+
+	strval = bc_num2str(resultnum);
+	*length = strlen(strval);
+	fprintf(stderr, "POW, length: %d\n", length);
+	if(*length <= 766) {
+		memset(result,0,766);
+		strncpy(result,strval,*length);
+		free(strval);
+		strval  = result;
+	}
+
+	bc_free_num(&tmpnum1);
+	bc_free_num(&tmpnum2);
+	bc_free_num(&resultnum);
+
+	return strval;
+}
+
+my_bool bcdiv_init(UDF_INIT *initid, UDF_ARGS *args, char* message) {
+	int i;
+	long scale;
+	if(args->arg_count != 3) {
+		strcpy(message,"This function requires at least two arguments.  bcadd(scale, arg, arg)"); 
+		return 1;
+	}
+
+	if (args->arg_type[0] != INT_RESULT) { 
+		strcpy(message, "First argument must be an INTEGER which represents the scale of the operation!");
+		return 1;
+	} else {
+ 		scale = (long)*(args->args[0]);
+	}
+
+	initid->max_length = 1024 * 1024;
+	for(i=1;i<args->arg_count;i++) {
+		args->arg_type[i] = STRING_RESULT;
+	}
+	return 0;
+}
+
+char *bcdiv(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long *length, char *is_null, char *error) {
+	bc_num tmpnum1;
+	bc_num tmpnum2;
+	bc_num resultnum;
+	char *strval;
+	char *str;
+	char *p;
+	int i;
+	bc_init_numbers();
+
+	bc_init_num(&tmpnum1);
+	bc_init_num(&tmpnum2);
+	bc_init_num(&resultnum);
+
+	str = args->args[1];
+	for(i=0;i<strlen(str);++i) {
+		if(*(str+i) < 48 || *(str+i) > 57) {
+			*(str+i)=0;
+		}
+	}
+	if (!(p = strchr(str, '.'))) {
+		bc_str2num(&tmpnum1, str, 0);
+	} else {
+		bc_str2num(&tmpnum1, str, strlen(p+1));
+	}
+
+	str = args->args[2];
+	for(i=0;i<strlen(str);++i) {
+		if(*(str+i) < 48 || *(str+i) > 57) {
+			*(str+i)=0;
+		}
+	}
+	if (!(p = strchr(str, '.'))) {
+		bc_str2num(&tmpnum2, str, 0);
+	} else {
+		bc_str2num(&tmpnum2, str, strlen(p+1));
+	}
+
+	if(*(str+1) == '0') {
+		// return NULL on division by zero
+		*is_null = 1;
+		memset(result, 0, 766);
+		return result;
+	}
+
 	bc_divide(tmpnum1,tmpnum2,&resultnum,(long)*(args->args[0])); 
 
 	strval = bc_num2str(resultnum);
-	length = strlen(strval);
-	if(length <= 766) {
+	*length = strlen(strval);
+	if(*length <= 766) {
 		memset(result,0,766);
-		strncpy(result,strval,length);
+		strncpy(result,strval,*length);
 		free(strval);
 		strval  = result;
 	}
@@ -269,7 +356,7 @@ my_bool bcmul_init(UDF_INIT *initid, UDF_ARGS *args, char* message) {
 	return 0;
 }
 
-char *bcmul(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length, char *is_null, char *error) {
+char *bcmul(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long *length, char *is_null, char *error) {
 	bc_num tmpnum1;
 	bc_num tmpnum2;
 	bc_num resultnum;
@@ -310,11 +397,11 @@ char *bcmul(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length
 	bc_multiply(tmpnum1,tmpnum2,&resultnum,(long)*(args->args[0])); 
 
 	strval = bc_num2str(resultnum);
-	length = strlen(strval);
+	*length = strlen(strval);
 
-	if(length <= 766) {
+	if(*length <= 766) {
 		memset(result,0,766);
-		strncpy(result,strval,length);
+		strncpy(result,strval,*length);
 		free(strval);
 		strval  = result;
 	}
@@ -347,7 +434,7 @@ my_bool bcsub_init(UDF_INIT *initid, UDF_ARGS *args, char* message) {
 	return 0;
 }
 
-char *bcsub(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length, char *is_null, char *error) {
+char *bcsub(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long *length, char *is_null, char *error) {
 	bc_num tmpnum1;
 	bc_num tmpnum2;
 	bc_num resultnum;
@@ -388,10 +475,10 @@ char *bcsub(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length
 	bc_sub(tmpnum1,tmpnum2,&resultnum,(long)*(args->args[0])); 
 
 	strval = bc_num2str(resultnum);
-	length = strlen(strval);
-	if(length <= 766) {
+	*length = strlen(strval);
+	if(*length <= 766) {
 		memset(result,0,766);
-		strncpy(result,strval,length);
+		strncpy(result,strval,*length);
 		free(strval);
 		strval  = result;
 	}
@@ -482,7 +569,7 @@ void bcsum_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
 }
 
 
-char *bcsum(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length, char *is_null, char *error) {
+char *bcsum(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long *length, char *is_null, char *error) {
 	char *strval;
 	if(initid->ptr != NULL) {
 		strval = bc_num2str((bc_num)*((bc_num*)(initid->ptr)));
@@ -491,19 +578,19 @@ char *bcsum(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long length
 		return result;	
 	}
 
-	length = strlen(strval);
-	if(length == 0) {
+	*length = strlen(strval);
+	if(*length == 0) {
 		*is_null=1; 
 		return result;
 	}
 
-	if(length > 766) {
+	if(*length > 766) {
 		// use our pointer
 		return(strval);
 	} else {
 		// use the preallocated space
 		memset(result,0,766);
-		strncpy(result, strval,length);
+		strncpy(result, strval,*length);
 		free(strval);
 		return result;
 	}
@@ -518,117 +605,6 @@ void bcsum_reset(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)
 }
 
 /*
-PHP_FUNCTION(bcsub)
-{
-	char *left, *right;
-	int left_len, right_len;
-	long scale_param = 0;
-	bc_num first, second, result;
-	int scale = BCG(bc_precision), argc = ZEND_NUM_ARGS();
-
-	if (zend_parse_parameters(argc, "ss|l", &left, &left_len, &right, &right_len, &scale_param) == FAILURE) {
-		return;
-	}
-	
-	if (argc == 3) {
-		scale = (int) ((int)scale_param < 0) ? 0 : scale_param;
-	}
-
-	bc_init_num(&first);
-	bc_init_num(&second);
-	bc_init_num(&result);
-	bc_str2num(&first, left);
-	bc_str2num(&second, right);
-	bc_sub (first, second, &result, scale);
-
-	if (result->n_scale > scale) {
-		result->n_scale = scale;
-	}
-
-	Z_STRVAL_P(return_value) = bc_num2str(result);
-	Z_STRLEN_P(return_value) = strlen(Z_STRVAL_P(return_value));
-	Z_TYPE_P(return_value) = IS_STRING;
-	bc_free_num(&first);
-	bc_free_num(&second);
-	bc_free_num(&result);
-	return;
-}
-PHP_FUNCTION(bcmul)
-{
-	char *left, *right;
-	int left_len, right_len;
-	long scale_param = 0;
-	bc_num first, second, result;
-	int scale = BCG(bc_precision), argc = ZEND_NUM_ARGS();
-
-	if (zend_parse_parameters(argc, "ss|l", &left, &left_len, &right, &right_len, &scale_param) == FAILURE) {
-		return;
-	}
-	
-	if (argc == 3) {
-		scale = (int) ((int)scale_param < 0) ? 0 : scale_param;
-	}
-	
-	bc_init_num(&first);
-	bc_init_num(&second);
-	bc_init_num(&result);
-	strtonum(&first, left);
-	strtonum(&second, right);
-	bc_multiply (first, second, &result, scale);
-
-	if (result->n_scale > scale) {
-		result->n_scale = scale;
-	}
-
-	Z_STRVAL_P(return_value) = bc_num2str(result);
-	Z_STRLEN_P(return_value) = strlen(Z_STRVAL_P(return_value));
-	Z_TYPE_P(return_value) = IS_STRING;
-	bc_free_num(&first);
-	bc_free_num(&second);
-	bc_free_num(&result);
-	return;
-}
-PHP_FUNCTION(bcdiv)
-{
-	char *left, *right;
-	int left_len, right_len;
-	long scale_param = 0;
-	bc_num first, second, result;
-	int scale = BCG(bc_precision), argc = ZEND_NUM_ARGS();
-
-	if (zend_parse_parameters(argc, "ss|l", &left, &left_len, &right, &right_len, &scale_param) == FAILURE) {
-		return;
-	}
-	
-	if (argc == 3) {
-		scale = (int) ((int)scale_param < 0) ? 0 : scale_param;
-	}
-	
-	bc_init_num(&first);
-	bc_init_num(&second);
-	bc_init_num(&result);
-	strtonum(&first, left);
-	strtonum(&second, right);
-
-	switch (bc_divide(first, second, &result, scale)) {
-		case 0: // OK 
-			if (result->n_scale > scale) {
-				result->n_scale = scale;
-			}
-			Z_STRVAL_P(return_value) = bc_num2str(result);
-			Z_STRLEN_P(return_value) = strlen(Z_STRVAL_P(return_value));
-			Z_TYPE_P(return_value) = IS_STRING;
-			break;
-		case -1: // division by zero 
-			php_error_docref(NULL, E_WARNING, "Division by zero");
-			break;
-	}
-
-	bc_free_num(&first);
-	bc_free_num(&second);
-	bc_free_num(&result);
-	return;
-}
 PHP_FUNCTION(bcmod)
 {
 	char *left, *right;
