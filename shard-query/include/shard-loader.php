0b264275ff456fab3155ef6b5c1d3feae0ba8d8e
Justin Swanhart <greenlion@gmail.com>
2014-10-24 23:49:36 +0000

    LOAD DATA LOCAL should be used

--- shard-query/include/shard-loader.php
+++ shard-query/include/shard-loader.php
@@ -630,7 +630,7 @@ class ShardLoader {
     if($replace == "") $replace=""; else $replace = "REPLACE";
 
     #note $ignore and replace are mutually exclusive in the SQL grammar - putting them together produces a error if both are used which is good
-    $load = "LOAD DATA INFILE \"$path\" {$replace}{$ignore} INTO TABLE `{$shard['db']}`.`$table`";
+    $load = "LOAD DATA LOCAL INFILE \"$path\" {$replace}{$ignore} INTO TABLE `{$shard['db']}`.`$table`";
     if($this->charset !== "" && $this->charset !== null) $load .= " CHARACTER SET {$this->charset} ";
     $sql  = "";
     
