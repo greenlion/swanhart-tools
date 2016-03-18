e6e1ce13dc25cd957a9cc9862afc3cf92b35a195
root <root@userdb-1-004.ody.live.las1.mz-inc.com>
2016-03-18 19:18:20 +0000

    Encode MySQL ROW as UTF8 for json_encode

--- flexviews/consumer/include/json_file.php
+++ flexviews/consumer/include/json_file.php
@@ -5,7 +5,16 @@ class json_file implements FlexCDC_Plugin_Interface {
 	static $path = "";
 	static $add_metadata = false;
 	static $fp = false;
-	static $buffer = "";
+	static $buffer = array();
+	static $filter = array();
+
+	static function to_utf8($dat) { 
+		if (is_string($dat)) return utf7_encode($dat); 
+		if (!is_array($dat)) return $dat; 
+		$ret = array(); 
+		foreach($dat as $i=>$d) $ret[$i] = utf8_encode_all($d); 
+		return $ret; 
+	} 
 
 	static function open() {
 		json_file::$fp = false;
@@ -23,7 +32,7 @@ class json_file implements FlexCDC_Plugin_Interface {
 			exit(2);
 		}
 		json_file::$fp = false;
-		json_file::$buffer = "";
+		json_file::$buffer = array();
 		return true;
 	}
 
@@ -35,9 +44,33 @@ class json_file implements FlexCDC_Plugin_Interface {
 			json_file::$add_metadata = true;
 		}
 
+		if(isset($instance->settings['json_file']['filter'])) {
+			$tmp = explode(',',$instance->settings['json_file']['filter']);
+			foreach($tmp as $tbl) {
+				$t2 = explode('.', $tbl);
+				if(count($t2) != 2) {
+					echo "JSON_FILE_INIT - Invalid schema filter: filter must be schema.table\n";
+					exit(4);
+				}
+				json_file::$filter[] = $t2;
+			} 
+		}
+
 		echo "JSON_FILE_INIT - Writing to file: " . json_file::$path . "\n";
 		echo "JSON_FILE_INIT - Adding metadata: " . (json_file::$add_metadata == true ? 'TRUE' : 'FALSE') . "\n";
-		
+		if(!empty(json_file::$filter)) {
+			echo "JSON_FILE_INIT - Filters:\n" . print_r(json_file::$filter,true);
+		}
+	}
+
+	static function skip_table($schema, $table) {
+		foreach(json_file::$filter as $filt) {
+			if($filt[0] == $schema && $filt[1] == $table) {
+				echo "SKIPPING $schema.$table\n";
+				return true;
+			}
+		}
+		return false;
 	}
 
 	static function plugin_deinit($instance) {
@@ -50,6 +83,7 @@ class json_file implements FlexCDC_Plugin_Interface {
 	}
 
 	static function commit_trx($uow_id, $gsn,$instance) {
+		if(empty(json_file::$buffer)) return;
 		foreach(json_file::$buffer as $line) {
 			if(!fwrite(json_file::$fp, $line)) {
 				echo "JSON_FILE_WRITE - error writing to file\n";
@@ -65,6 +99,7 @@ class json_file implements FlexCDC_Plugin_Interface {
 	}
 
 	static function insert($row, $db, $table, $trx_id, $gsn,$instance) {
+		if(json_file::skip_table($db, $table)) return;
 		$row['MYSQL_ACTION'] = 'I';
 		if(json_file::$add_metadata) {
 			$row['MYSQL_TABLE'] = $table;
@@ -72,10 +107,11 @@ class json_file implements FlexCDC_Plugin_Interface {
 			$row['MYSQL_TRX'] = $trx_id;
 			$row['MYSQL_SEQ'] = $gsn;
 		}
-		json_file::$buffer[] = json_encode($row) . "\n";
+		json_file::$buffer[] = json_encode(json_file::to_utf8($row)) . "\n";
 	}
 	
 	static function delete($row, $db, $table, $trx_id, $gsn,$instance) {
+		if(json_file::skip_table($db, $table)) return;
 		$row['MYSQL_ACTION'] = 'D';
 		if(json_file::$add_metadata) {
 			$row['MYSQL_TABLE'] = $table;
@@ -83,10 +119,11 @@ class json_file implements FlexCDC_Plugin_Interface {
 			$row['MYSQL_TRX'] = $trx_id;
 			$row['MYSQL_SEQ'] = $gsn;
 		}
-		json_file::$buffer[] = json_encode($row) . "\n";
+		json_file::$buffer[] = json_encode(json_file::to_utf8($row)) . "\n";
 	}
 	
 	static function update_before($row, $db, $table, $trx_id, $gsn,$instance) {
+		if(json_file::skip_table($db, $table)) return;
 		$row['MYSQL_ACTION'] = 'UB';
 		if(json_file::$add_metadata) {
 			$row['MYSQL_TABLE'] = $table;
@@ -94,10 +131,12 @@ class json_file implements FlexCDC_Plugin_Interface {
 			$row['MYSQL_TRX'] = $trx_id;
 			$row['MYSQL_SEQ'] = $gsn;
 		}
-		json_file::$buffer[] = json_encode($row) . "\n";
+		echo "TABLE: $db.$table\n";
+		json_file::$buffer[] = json_encode(json_file::to_utf8($row)) . "\n";
 	}
 
 	static function update_after($row, $db, $table, $trx_id, $gsn,$instance) {
+		if(json_file::skip_table($db, $table)) return;
 		$row['MYSQL_ACTION'] = 'UA';
 		if(json_file::$add_metadata) {
 			$row['MYSQL_TABLE'] = $table;
@@ -105,6 +144,6 @@ class json_file implements FlexCDC_Plugin_Interface {
 			$row['MYSQL_TRX'] = $trx_id;
 			$row['MYSQL_SEQ'] = $gsn;
 		}
-		json_file::$buffer[] = json_encode($row) . "\n";
+		json_file::$buffer[] = json_encode(json_file::to_utf8($row)) . "\n";
 	}
 }
