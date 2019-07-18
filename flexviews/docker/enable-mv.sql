1e79bb9df4c8b603f43ad140da11c209298b63b3
kurokaway <krkwy.sk.dev@gmail.com>
2019-07-18 19:59:24 +0800

    Dockerize flexviews

---
+++ flexviews/docker/enable-mv.sql
@@ -0,0 +1,4 @@
+call flexviews.create_mvlog('demo', 'orders');
+call flexviews.create_mvlog('demo', 'customers');
+call flexviews.create_mvlog('demo','order_details');
+call flexviews.create_mvlog('demo','products');
